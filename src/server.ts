864b92b2021dcd5903c6e43d59c9bea134f7f33a
Alaa Rihan <alaa.addeen@gmail.com>
2021-08-06 09:37:42 +0300

    Serve static to see the taken screenshot

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import fastify from 'fastify'
+import Handlebars from 'handlebars'
+import path from 'path'
 import { runSurfing } from './tasks/surfing'
 import { takeScreenshot } from './tasks/takeScreenshot'
 import { cacheStore } from './utils/cacheStore'
@@ -10,6 +12,47 @@ app.register(require('fastify-cors'), {
   credentials: true,
 })
 app.register(require('fastify-cookie'))
+
+// Handlebar template for listing files and directories.
+const template = `
+<html>
+  <body>
+  {{#if dirs}}
+    dirs
+  <ul>
+    {{#dirs}}
+      <li><a href="/public{{href}}">{{name}}</a></li>
+    {{/dirs}}
+  </ul>
+  {{/if}}
+  {{#if files}}
+  list
+  <ul>
+    {{#files}}
+      <li><a href="/public{{href}}" target="_blank">{{name}}</a></li>
+    {{/files}}
+  </ul>
+  {{/if}}
+  </body>
+</html>
+`
+const handlebarTemplate = Handlebars.compile(template)
+app.register(require('fastify-static'), {
+    // An absolute path containing static files to serve.
+    root: path.join(__dirname, '../public'),
+    prefix: '/public/',
+    prefixAvoidTrailingSlash: true,
+    // Return a directory listing with a handlebar template.
+    list: {
+      // html or json response? html requires a render method.
+      format: 'html',
+      // A list of filenames that trigger a directory list response.
+      names: ['index', 'index.html', 'index.htm', '/'],
+      // You can provide your own render method as needed.
+      render: (dirs, files) => handlebarTemplate({ dirs, files })
+    }
+  })
+
 
 app.get('/run/surfing', async (req, reply) => {
   if (req.query['secret'] !== process.env.APP_SECRET) {
@@ -23,12 +66,12 @@ app.get('/run/surfing', async (req, reply) => {
 })
 
 app.get('/run/screenshot', async (req, reply) => {
-    if (req.query['secret'] !== process.env.APP_SECRET) {
-      return reply.status(403).send('Unauthorized!')
-    }
-    takeScreenshot()
-    return reply.status(200).send('Taking a screenshot.')
-  })
+  if (req.query['secret'] !== process.env.APP_SECRET) {
+    return reply.status(403).send('Unauthorized!')
+  }
+  takeScreenshot()
+  return reply.status(200).send('Taking a screenshot.')
+})
 
 app
   .listen(3001)
--- src/tasks/takeScreenshot.ts
+++ src/tasks/takeScreenshot.ts
@@ -4,10 +4,10 @@ import { createLog } from '../utils/createLog'
 
 export async function takeScreenshot() {
   const browser = cacheStore.get('browser') as Browser
-  if(browser || browser.isConnected()){
+  if(browser?.isConnected()){
     const pages = await browser.pages()
     const page = pages[pages.length - 1] as Page
     createLog('Take a screenshot')
-    page.screenshot({path: 'screenshot.png'})
+    page.screenshot({path: `./public/screenshot-${new Date().toString()}.png`})
   }
 }
