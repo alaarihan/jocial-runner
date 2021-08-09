4e24e5bd463a563384935adcb74465e5e7c8fe84
Alaa Rihan <alaa.addeen@gmail.com>
2021-08-09 22:17:52 +0300

    Small improvement

--- src/tasks/loginActivity.ts
+++ src/tasks/loginActivity.ts
@@ -91,7 +91,7 @@ export async function runLoginActivity(account = null) {
 async function loginActivityCheck(page: Page, loop = 1) {
   const account = cacheStore.get('account') as any
   await page.waitForTimeout(180000)
-  if (loop > 52) {
+  if (loop > 42) {
     createLog(`Logging account ${account.name} out`)
     await page.click('.uesrName')
     await page.waitForTimeout(1000)
